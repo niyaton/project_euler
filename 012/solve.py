381d925e2ec9c91334bf135e46a7be6d32a5f98d
Kenji Fujiwara <niyaton@gmail.com>
2013-06-30 17:01:26 +0900

    解いてる途中その4

--- 012/solve.py
+++ 012/solve.py
@@ -1,27 +1,18 @@
-from itertools import count
-from itertools import izip
+import math
+def get_largest_factor(num):
+    max = int(math.sqrt(num))
+    for i in range(max, 1, -1):
+        if num % i == 0:
+            return i
 
 def get_factors(num):
-    for i in range(2, num/2 + 1):
+    max = int(math.sqrt(num))
+    for i in range(max, 1, -1):
         if num % i == 0:
             yield i
 
-def get_num_factors(num):
-    result = 2
-    for i in range(2, num/2 + 1):
-        if num % i == 0:
-            result += 1
-    return result
-
-def gen_triangle():
-    sum = 0
-    for i in count(1):
-        sum += i
-        yield sum
-
-print list(get_factors(500))
-#for i, triangle in izip(count(), gen_triangle()):
-#    num_factors = get_num_factors(triangle)
-#    if num_factors > 500:
-#        print i , triangle, num_factors
-#        break
+print list(get_factors(13195))
+for i in get_factors(600851475143):
+    if len(list(get_factors(i))) == 0:
+        print i
+        exit(0)
