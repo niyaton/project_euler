4eb67041a76635187cd6606f26aa32db03743ade
Kenji Fujiwara <niyaton@gmail.com>
2013-06-30 16:20:06 +0900

    refactored solution of problem 011

--- 011/solve.py
+++ 011/solve.py
@@ -1,30 +1,17 @@
 nums = []
 for line in open('input').readlines():
-    line = line.strip()
     nums.append([ int(i) for i in line.split(' ') ])
 
-products = []
-for x in range(20):
-    for y in range(20):
-        p = [1, 1, 1, 1]
-        for i in range(4):
-            if y + i >= 20:
-                p[0] = 0
-            else:
-                p[0] *= nums[x][y+i]
-            if x + i >= 20:
-                p[1] = 0
-            else:
-                p[1] *= nums[x+i][y]
-            if x + i >= 20 or y + i >= 20:
-                p[2] = 0
-            else:
-                p[2] *= nums[x+i][y+i]
-            if x - i< 0 or y + i >= 20:
-                p[3] = 0
-            else:
-                p[3] *= nums[x-i][y+i]
-        products.extend(p)
+def get_products():
+    for x in range(20):
+        for y in range(20):
+            p = [1, 1, 1, 1]
+            for i in range(4):
+                p[0] *= nums[x][y+i] if y + i < 20 else 0
+                p[1] *= nums[x+i][y] if x + i < 20 else 0
+                p[2] *= nums[x+i][y+i] if x + i < 20 and y + i < 20 else 0
+                p[3] *= nums[x-i][y+i] if x - i >= 0 and y + i < 20 else 0
+            for i in p:
+                yield i
 
-print max(products)
-
+print max(get_products())
