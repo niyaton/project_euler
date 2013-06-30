01cc9c1143bc01e5edeb214f53468647ab409785
Kenji Fujiwara <niyaton@gmail.com>
2013-06-30 15:38:57 +0900

    problem 008を間違った方針で解いていたなごりその２

--- 008/solve.py
+++ 008/solve.py
@@ -1,15 +1,34 @@
-from itertools import count
-l = []
-c = count(1)
-for i in range(5):
-    l.append(c.next())
+import math
+#print '9'*1000
+#print 1.0E999
+#max = int('9'*1000)
+#print max
 
-for i in c:
-    #print l,
+#print 1000 / 5
+
+i = int('1' + '0'*200)
+print i*i*i*i*i
+print len(str(i*i*i*i*i))
+
+#print i*(i-1)*(i-2)*(i-3)*(i-4)
+#print len(str((i*(i-1)*(i-2)*(i-3)*(i-4))))
+#print len(str(((i+1)*(i-1)*(i-2)*(i-3)*(i-4))))
+
+s = i - 2
+l = []
+for i in range(5):
+    l.append(s+i)
+#print reduce(lambda a,b: a*b, l)
+
+from itertools import count
+for i in count():
     p = reduce(lambda a,b: a*b, l)
     if len(str(p)) > 1000:
         break
-    ans = (p,l)
+    print p
+    print len(str(p))
     l.pop(0)
-    l.append(i)
-print ans
+    l.append(l[-1]+1)
+    print l
+    
+
