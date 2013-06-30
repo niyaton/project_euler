736a2603bbb44d97d46686137e73f59994880cd1
Kenji Fujiwara <niyaton@gmail.com>
2013-06-30 17:01:41 +0900

    解いてる途中その5

--- 012/solve.py
+++ 012/solve.py
@@ -1,18 +1,50 @@
 import math
-def get_largest_factor(num):
-    max = int(math.sqrt(num))
-    for i in range(max, 1, -1):
-        if num % i == 0:
-            return i
+from itertools import permutations, izip
+import sys
 
-def get_factors(num):
-    max = int(math.sqrt(num))
-    for i in range(max, 1, -1):
-        if num % i == 0:
-            yield i
+primes = [2, 3, 5, 7, 9, 11, 13, 17, 23, 29, 31, 37, 41, 43, 47, 53, 59]
 
-print list(get_factors(13195))
-for i in get_factors(600851475143):
-    if len(list(get_factors(i))) == 0:
-        print i
-        exit(0)
+def is_triangle(num):
+    n = (math.sqrt(8*num + 1) - 1)/2
+    if n == int(n):
+        print int(n)
+    return n == int(n)
+
+#while True:
+#    line = sys.stdin.readline().strip()
+#    print is_triangle(int(line))
+ans = set()
+#for i in permutations(primes, 5):
+#    product = 1
+#    for p, exp in izip(i, l):
+#        product *= math.pow(p,exp)
+#    if is_triangle(product):
+#        print i, product, is_triangle(product)
+#        ans.add(product)
+
+def search(l):
+    for i in permutations(primes, len(l)):
+        product = 1
+        for p, exp in izip(i, l):
+            product *= math.pow(p,exp)
+        if is_triangle(product):
+            print i, product, is_triangle(product)
+            ans.add(product)
+
+#l = [1, 1, 4, 4, 4]
+#l = [3, 4, 4, 4]
+#search(l)
+
+l = [1, 1, 1, 2, 2, 6]
+search(l)
+l = [3, 1, 2, 2, 6]
+search(l)
+l = [3, 5, 2, 6]
+search(l)
+
+
+
+#l = [1, 10, 22]
+#search(l)
+
+print min(ans)
