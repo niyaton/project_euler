1eb5194dbdc500b7ec84aa623fec8e6f49b32d53
Kenji Fujiwara <niyaton@gmail.com>
2013-06-30 15:51:53 +0900

    improved performance of problem 010

--- 010/solve.py
+++ 010/solve.py
@@ -1,10 +1,7 @@
 import math
-from itertools import count
 
 def is_prime_num(num):
     max = int(math.sqrt(num))
-    if num == 2:
-        return True
     for i in xrange(3, max+1, 2):
         if num % i == 0:
             return False
@@ -12,9 +9,7 @@ def is_prime_num(num):
 
 def get_primes():
     yield 2
-    for i in count(3,2):
-        if i > 2000000:
-            break
+    for i in xrange(3, 2000000, 2):
         if is_prime_num(i):
             yield i
 
